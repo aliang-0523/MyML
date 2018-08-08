fc0ca271ec56db8be9a58d563c9dde071ba27c54
sjsj19960523 <626591833@qq.com>
2018-08-08 14:09:34 +0800

    vectorize

--- Linear regression.py
+++ Linear regression.py
@@ -2,18 +2,21 @@ from numpy import *;
 import numpy as np
 x=np.matrix([[1,1],[1,2],[1,3]]);
 y=np.matrix([[1],[2],[3]]);
-theta=0.5;
-temp=0;
-h=mat([[1],[1],[1]])
+theta=np.matrix([[0.5]]);
+temp=np.matrix([[0.0]]);
 sum=0;
+h=mat([[1],[1],[1]])
 count=0;
-while(abs(theta-temp)>0.01):
+times=0;
+while(abs(theta.A[0]-temp.A[0])>0.00000001):
     for a in x[:, 1]:
         b=a.A;
-        sum+=b[0]*theta*b[0]-b[0]*y.A[count];
+        sum+=b[0]*theta.A[0]*b[0]-b[0]*y.A[count];
         count+=1;
     sum=sum/3;
     count=0;
-    temp=theta;
-    theta=theta-0.1*sum;
-print(theta);
+    temp.A[0]=theta.A[0];
+    theta.A[0]=theta.A[0]-0.1*sum;
+    times+=1;
+print(theta.A[0]);
+print(times);
