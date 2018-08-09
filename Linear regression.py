b56e433442e65ea7d9e8054f2481c5c333d9c93a
sjsj19960523 <626591833@qq.com>
2018-08-09 10:33:11 +0800

    参数化

--- Linear regression.py
+++ Linear regression.py
@@ -5,21 +5,21 @@ y=np.matrix([[1],[2],[3]]);
 theta=np.matrix([[0.5]]);
 temp=np.matrix([[0.0]]);
 '判断theta值是否收敛'
-def compare(the,tem):
+def compare(the,tem,slrange):
     countn = 0;
     for i in the.A:
-        if(abs(i-tem.A[np.where(the.A==i)])>0.000000001):
+        if(abs(i-tem.A[np.where(the.A==i)])>slrange):
             countn+=1;
     if(countn==the.A.size):
         return True;
     else:
         return False;
 '线性回归算法'
-def linear_regression():
+def linear_regression(learning_rate,slrange):
     sum = 0;
     count = 0;
     times = 0;
-    while(compare(theta,temp)):
+    while(compare(theta,temp,slrange)):
         for a in x[:, 1]:
             b=a.A;
             'jtheta对theta(i)的偏导的和'
@@ -28,9 +28,9 @@ def linear_regression():
         sum=sum/y.A.size;
         count=0;
         temp.A[0]=theta.A[0];
-        theta.A[0]=theta.A[0]-0.1*sum;
+        theta.A[0]=theta.A[0]-learning_rate*sum;
         times+=1;
     print(theta.A[0]);
     print(times);
 if __name__ == '__main__':
-    linear_regression();
+    linear_regression(0.1,0.000000001);
