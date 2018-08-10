43c6f28248174524595b045a504db386eb617692
sjsj19960523 <626591833@qq.com>
2018-08-10 09:20:59 +0800

    优化

--- Linear regression.py
+++ Linear regression.py
@@ -1,5 +1,6 @@
 from numpy import *;
 import numpy as np
+import math
 x=np.matrix([[1,1],[1,2],[1,3]]);
 y=np.matrix([[1],[2],[3]]);
 theta=np.matrix([[0.1],[0.4]]);
@@ -11,8 +12,7 @@ def compare(the,tem,slrange):
     for i in range(0, len(the.A)):
         if(abs(the.A[i] - tem.A[i])>slrange):
             countn[i]+=1;
-    for z in range(0, len(the.A)):
-        if(countn[z]==1):
+        if(countn[i]==1):
             delta+=1;
     if(delta==len(the.A)):
         return True;
@@ -27,18 +27,18 @@ def linear_regression(learning_rate,slrange):
         for a in x[:, 1]:
             b=a.A;
             'jtheta对theta(i)的偏导的和'
-            sum[0]+=theta.A[0]+b[0]*theta.A[1]-y.A[count];
-            sum[1]+=b[0]*(theta.A[0]+b[0]*theta.A[1]-y.A[count]);
+            for i in range(0, len(sum)):
+                '模型改变之后hypothesis(theta)的计算方式也需要改变'
+                sum[i]+=math.pow(b[0],i)*(theta.A[0]+b[0]*theta.A[1]-y.A[count]);
             count+=1;
         for i in range(0, len(sum)):
             sum[i]=sum[i]/y.A.size;
-        count=0;
-        for i in range(0, len(temp.A)):
             temp.A[i] = theta.A[i];
             theta.A[i] = theta.A[i] - learning_rate * sum[i];
+        count=0;
         times+=1;
     print(theta.A[0]);
     print(theta.A[1]);
     print(times);
 if __name__ == '__main__':
-    linear_regression(0.2,0.000000001);
+    linear_regression(0.32,0.000000000001);
