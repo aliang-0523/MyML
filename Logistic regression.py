2ecb4da00efd193107845c8bf608985c91f821aa
sjsj19960523 <626591833@qq.com>
2018-08-10 14:17:16 +0800

    Jtheta检验收敛

--- Logistic regression.py
+++ Logistic regression.py
@@ -1,12 +1,20 @@
 from numpy import *;
 import numpy as np
 import math
-x=np.matrix([[1,1],[1,2],[1,3],[1,4],[1,5],[1,6],[1,7],[1,8]]);
-y=np.matrix([[1],[2],[3],[4],[5],[6],[7],[8]]);
+x=np.matrix([[1,1],[1,2],[1,3]]);
+y=np.matrix([[1],[0],[1]]);
 theta=np.matrix([[0.1],[0.4]]);
-'判断Jtheta值是否收敛'
-def compare(Jtheta,temp,slrange):
-    if(abs(Jtheta-temp)>slrange):
+temp=np.matrix([[0.0],[0.1]]);
+'判断theta值是否收敛'
+def compare(the,tem,slrange):
+    countn = [0,0];
+    delta=0;
+    for i in range(0, len(the.A)):
+        if(abs(the.A[i] - tem.A[i])>slrange):
+            countn[i]+=1;
+        if(countn[i]==1):
+            delta+=1;
+    if(delta==len(the.A)):
         return True;
     else:
         return False;
@@ -14,9 +22,7 @@ def compare(Jtheta,temp,slrange):
 def linear_regression(learning_rate,slrange):
     count = 0;
     times = 0;
-    temp = 0;
-    Jtheta = 1;
-    while(compare(Jtheta,temp,slrange)):
+    while(compare(theta,temp,slrange)):
         sum = [0.0,0.0];
         for a in x[:, 1]:
             b=a.A;
@@ -26,10 +32,9 @@ def linear_regression(learning_rate,slrange):
                 'sum[i]+=math.pow(b[0],i)*(theta.A[0]+b[0]*theta.A[1]-y.A[count]);'
                 sum[i] += math.pow(b[0], i) * hypothesis(theta,b[0],y.A[count]);
             count+=1;
-        temp = Jtheta;
-        Jtheta = jtheta(x[:, 1].A, y.A, theta);
         for i in range(0, len(sum)):
             sum[i]=sum[i]/y.A.size;
+            temp.A[i] = theta.A[i];
             theta.A[i] = theta.A[i] - learning_rate * sum[i];
         count=0;
         times+=1;
@@ -38,10 +43,5 @@ def linear_regression(learning_rate,slrange):
     print(times);
 def hypothesis(theta,x,y):
     return theta.A[0]+x*theta.A[1]-y;
-def jtheta(x,y,theta):
-    sum=0;
-    for i in range(0, len(y)):
-        sum+=math.pow(theta.A[0]+theta.A[1]*x[i]-y[i],2);
-    return sum;
 if __name__ == '__main__':
-    linear_regression(0.019,0.00000001);
+    linear_regression(0.32,0.000000000001);
